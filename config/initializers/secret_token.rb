528ac35e5a7da38a5d6286e8eaea2f6ef541c4aa
Juan Maiz <juanmaiz@gmail.com>
2011-05-27 08:29:48 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rsonrails::Application.config.secret_token = 'b54b2a47eade19afaca27fd198fc1cb3ba246fb8b2ad89d48024ef62fbd96b93643721536662029174a913845292bc02886d984d1f35503f6eb8cdc46b295f82'
