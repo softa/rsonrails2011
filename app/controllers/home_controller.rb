0d8abafc8c502ba6342b7c604faea25693431770
Juan Maiz <juanmaiz@gmail.com>
2011-06-14 17:00:26 -0300

    versão decente 1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
+SPEAKERS =   [{:email => 'andrec@checkplant.com.br', :name => 'André Cantarelli & Frederico Fonseca', :company => 'checkplant.com.br'},
+  {:email => 'ccarneiroj@gmail.com', :name => 'Cloves Carneiro', :company => 'amazon.com'},
+  {:email => 'danicuki@gmail.com', :name => 'Daniel Cukier', :company => 'usp'},
+  {:email => 'daniel@egenial.com.br', :name => 'Daniel Lopes', :company => 'objetiva.co'},
+  {:email => 'diego@cuboxsa.com', :name => 'Diego Algorta*', :company => 'cubox.com.uy'},
+  {:email => 'everton.carpes@gmail.com', :name => 'Éverton Carpes', :company => 'globo.com'},
+  {:email => 'fabioakita@gmail.com', :name => 'Fábio Akita', :company => 'gonow.com.br'},
+  {:email => 'felipe.lima@gmail.com', :name => 'Felipe Lima', :company => 'thoughtworks.com'},
+  {:email => 'nicolas@quavio.com.br', :name => 'Nícolas Iensen', :company => 'quavio.com.br'},
+  {:email => 'rafael.lima@bielsystems.com.br', :name => 'Rafael Lima', :company => 'bielsystems.com.br'},
+  {:email => 'rafael.ssouza@gmail.com', :name => 'Rafael Souza', :company => 'softa.com.br'},
+  {:email => '', :name => 'Rodrigo Flores', :company => 'plataformatec.com.br'},
+  {:email => 'gabriel.sobrinho@gmail.com', :name => 'Gabriel Sobrinho', :company => ''},
+  {:email => '', :name => 'Thais Camilo', :company => 'hashrocket.com'}]
+
   def index
+    @speakers = SPEAKERS
   end
 
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+  def gravatar email
+    require 'digest/md5'
+    md5 = Digest::MD5.hexdigest(email)
+    image_tag "http://gravatar.com/avatar/#{md5}.jpg?s=420"
+  end
+  def tip text, rel
+    raw %(<a rel="##{rel}" class="tip">#{text}</a>)
+  end
+
 end
